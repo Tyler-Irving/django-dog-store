8d2b037216a1346323d39f024db0267792895d00
Student <student@donation.local>
2019-11-13 09:47:47 -0600

    Django Benchmark Update:
    'form.py' was added after reviewing the README.md. Nothing has been made
    
    Three classes have been named + passed in 'models.py' after reviewing
    the README.md

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class DogProduct(models.Model):
+    pass
+
+class Purchase(models.Model):
+    pass
+
+class DogTag(models.Model):
+    pass
