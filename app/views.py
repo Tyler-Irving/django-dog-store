c69f54928cc28c166178907078ebb095d80c37f3
Student <student@donation.local>
2019-11-13 10:09:44 -0600

    Updates to 'forms.py', 'views.py', and 'urls.py'
    'forms.py'
    Created one form class named
    'NewDogTagForm'. This class checks for owner_name, dog_name,
    dog_birthday
    
    'views.py'
    passed 6 functions through as needed by the README.
    
    'urls.py'
    Imported 'views.py' and made 6 paths, one for each view.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from app.models import DogProduct, Purchase, DogTag
+from app.forms import NewDogTagForm
 
 # Create your views here.
+def home(request):
+    pass
+
+
+def dog_product_detail(request):
+    pass
+
+
+def purchase_dog_product(request):
+    pass
+
+
+def purchase_detail(request):
+    pass
+
+
+def new_dog_tag(request):
+    pass
+
+
+def dog_tag_list(request):
+    pass
