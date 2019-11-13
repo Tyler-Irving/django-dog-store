d4af60300ad5763d9f05f8c4cab8d692fef8add6
Student <student@donation.local>
2019-11-13 11:23:25 -0600

    'views.py'/purchase_dog_product()
    This function gets the 'dog_product' associated with the kwarg provided.
    if the product is found to be in stock the 'dog_product' quantity is
    reduced by 1, a new Purchase is created, and the user is redirected to
    'purchase_detail.html at the id of the purchase.
    
    else if the product is not in stock, the user is prompeted the error
    message "{dog_product.name} is out of stock" and redirected to
    'purchase_detail.html at the id of the purchase.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
+from django.contrib import messages
 from app.models import DogProduct, Purchase, DogTag
 from app.forms import NewDogTagForm
 
@@ -14,13 +15,21 @@ def dog_product_detail(request, dog_product_id):
     return render(request, "dog_product_detail.html", {"dog_product": dog_product})
 
 
-def purchase_dog_product(request):
-    pass
+def purchase_dog_product(request, dog_product_id):
+    dog_product = DogProduct.objects.get(id=dog_product_id)
+    if dog_product.quantity > 0:
+        dog_product.quantity -= 1
+        purchase = Purchase.objects.create(
+            dog_product=dog_product, purchased_at=timezone.now()
+        )
+        return redirect("purchase_detail.html", purchase.id)
+    elif dog_product.quantity <= 0:
+        messages.error(f"{dog_product.name} is out of stock")
+        return redirect("dog_product_detail.html", dog_product_id)
+
 
 
 def purchase_detail(request):
-    pass
-
 
 def new_dog_tag(request):
     pass
