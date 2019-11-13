b9bdee1077aa7e13c714ef328907a0b80d375b74
Student <student@donation.local>
2019-11-13 11:46:54 -0600

    'views.py'/purchase_dog_product() and purchase_detail()
    Imports: from django.contrib import messages
    
    'purchase_dog_product()'
    Added the messages from django to display to the user if the purchase
    went through or not.
    Fixed the redirect so user could actually purchase items instead of
    hitting a '404 Not Found' wall
    
    purchase_detail()
    purchase identified by the kwarg and is set to the variable 'purchase'
    returns a render of "purchase_detail.html" at with the purchase

--- app/views.py
+++ app/views.py
@@ -19,17 +19,22 @@ def purchase_dog_product(request, dog_product_id):
     dog_product = DogProduct.objects.get(id=dog_product_id)
     if dog_product.quantity > 0:
         dog_product.quantity -= 1
+        dog_product.save()
         purchase = Purchase.objects.create(
             dog_product=dog_product, purchased_at=timezone.now()
         )
-        return redirect("purchase_detail.html", purchase.id)
+        purchase.save()
+        messages.success(request, f"Purchased {dog_product.name}")
+        return redirect("purchase_detail", purchase.id)
     elif dog_product.quantity <= 0:
-        messages.error(f"{dog_product.name} is out of stock")
-        return redirect("dog_product_detail.html", dog_product_id)
+        messages.error(request, f"{dog_product.name} is out of stock")
+        return redirect("dog_product_detail", dog_product_id)
 
 
+def purchase_detail(request, purchase_id):
+    purchase = Purchase.objects.get(id=purchase_id)
+    return render(request, "purchase_detail.html", {"purchase": purchase})
 
-def purchase_detail(request):
 
 def new_dog_tag(request):
     pass
